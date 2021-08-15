7ff143f1dc0d3c643a2250521b62b0ed1db6a258
Charles Willams <Charles.Williams@pyramidci.com>
2021-08-15 00:08:57 -0400

    first commit

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Calculate(string input)
+        {
+            DataTable dt = new DataTable();
+            var result = dt.Compute(input, "");
+            return result.ToString();
+
+        }
+
+    }
+
+}
---
+++ Models/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator.Models
+{
+    public class Calculator
+    {
+        public int operand1 { get; set; }
+        public int operand2 { get; set; }
+    }
+}
